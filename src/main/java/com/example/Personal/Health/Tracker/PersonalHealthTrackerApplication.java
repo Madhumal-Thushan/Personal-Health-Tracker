90517f78e635ecdcb37b5c55fbaae26aa2256964
Madhumal Thushan <madhumal.thushan@axiatadigitallabs.com>
2024-09-18 12:45:08 +0530

    init commit

---
+++ src/main/java/com/example/Personal/Health/Tracker/PersonalHealthTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Personal.Health.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalHealthTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalHealthTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Personal/Health/Tracker/PersonalHealthTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Personal.Health.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalHealthTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
